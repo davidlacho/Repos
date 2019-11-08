2906ba86d728a8505f1f2e75160b2dcaed852e4d
davidlacho <david.lacho@gmail.com>
2019-11-07 19:38:50 -0800

    displaying repos user is watching

--- web/channels/search_channel.ex
+++ web/channels/search_channel.ex
@@ -5,12 +5,6 @@ defmodule Repos.SearchChannel do
   end
 
   def handle_in(_, %{"username" => username}, socket) do
-    # %{"data" => %{"user" => %{"following" => %{"edges" => following},"issues" => %{"edges" => issues},"pinnedRepositories" => %{"edges" => pinned_repositories}, "pullRequests" => %{"edges" => pull_requests}, "watching" => %{"edges" => watching}}}} = Repos.Worker.get_user(username)
-    # IO.inspect(following)
-    # IO.inspect(Enum.count(issues))
-    # IO.inspect(pinned_repositories)
-    # IO.inspect(Enum.count(pull_requests))
-    # IO.inspect(watching)
     broadcast!(socket, "search:1:result", Repos.Worker.get_user(username))
     {:reply, :ok, socket}
   end
--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -17,6 +17,74 @@ channel.join()
   });
   
 channel.on("search:1:result", (resp) => {
-  console.log(resp);
+  if (resp.data.user) {
+      const issueCount = resp.data.user.issues.edges.length || 0;
+      const pullRequestCount = resp.data.user.pullRequests.edges.length || 0
+
+    // if (resp.data.user.following.edges && resp.data.user.following.edges.length > 0) {
+    //   resp.data.user.following.edges.forEach(node => {
+    //     console.log(node)
+    //   });
+    // }
+
+    // if (resp.data.user.pinnedRepositories.edges && resp.data.user.pinnedRepositories.edges.length > 0) {
+    //   resp.data.user.pinnedRepositories.edges.forEach(node => {
+    //     console.log(node)
+    //   });
+    // }
+
+    if (resp.data.user.watching.edges && resp.data.user.watching.edges.length > 0) {
+        const html = renderWatchingHtml(resp.data.user.watching.edges);
+        document.getElementById('user-data').innerHTML = '';
+        document.getElementById('user-data').innerHTML = html;
+    }
+
+  }
 })
+
+const renderWatchingHtml = watching => {
+  let html = '<div class="ui relaxed divided list">';
+  watching.forEach(w => {
+    html += `
+    <div class="item">
+    <i class="large github middle aligned icon"></i>
+    <div class="content">
+      <a class="header" href="${w.node.url}">${w.node.name}</a>
+      <div class="description">${w.node.description}</div>
+    </div>
+  </div>
+    `;
+  });
+  html += '</div>'
+  return html;
+}
+
+const renderCard = (issueCount, pullRequests) => {
+  return `<div class="card">
+  <div class="image">
+    <img src="/images/avatar2/large/matthew.png">
+  </div>
+  <div class="content">
+    <div class="header">Matt Giampietro</div>
+    <div class="meta">
+      <a>Friends</a>
+    </div>
+    <div class="description">
+      Matthew is an interior designer living in New York.
+    </div>
+  </div>
+  <div class="extra content">
+    <span class="right floated">
+      ${issueCount} issues.
+    </span>
+    <span>
+      <i class="user icon"></i>
+      ${pullRequestCount} pull requests.
+    </span>
+  </div>
+</div>`
+
+}
+
+
 export default socket
